8219f280dbca6bf14d89e5e7bebcb5045b3c28a9
brad <bwbazemore@uga.edu>
2016-11-02 08:47:09 -0400

    start

---
+++ Main.py
@@ -0,0 +1,9 @@
+import json
+import thunder as td
+from extraction import NMF
+
+files = [
+    '00.00.test', '00.01.test', '01.00.test',
+    '01.01.test', '02.00.test', '02.01.test',
+    '03.00.test', '04.00.test', '04.01.test'
+]
