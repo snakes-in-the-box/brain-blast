f4fb8e2d05d3d76cf3e45ded1b7758cc05667f5d
brad <bwbazemore@uga.edu>
2016-11-03 13:12:55 -0400

    fixed data write problem

--- Main.py
+++ Main.py
@@ -15,10 +15,10 @@ class BrainBlast:
     def learn_data(self):
         for d in self.data:
             path = self.folder + d
-            print("Analyzing on ", path)
-            data = td.images.fromtif(path + '/images', ext='tiff')
+            print "Analyzing " + path
+            image_data = td.images.fromtif(path + '/images', ext='tiff')
             algorithm = NMF(k=10, percentile=99, max_iter=50, overlap=0.1)
-            model = algorithm.fit(data, chunk_size=(50, 50), padding=(25, 25))
+            model = algorithm.fit(image_data, chunk_size=(50, 50), padding=(25, 25))
             merged = model.merge(0.1)
             regions = [{'coordinates': region.coordinates.tolist()} for region in merged.regions]
             result = {'dataset': self.data, 'regions': regions}
@@ -33,15 +33,7 @@ if __name__ == "__main__":
 
     folder_path = '/media/brad/disk2/nf_data_test/neurofinder.'
     files = [
-        '00.00.test',
-        '00.01.test',
-        '01.00.test',
-        '01.01.test',
-        '02.00.test',
-        '02.01.test',
-        '03.00.test',
-        '04.00.test',
-        '04.01.test'
+        '00.00.test'
     ]
     submission = []
 
